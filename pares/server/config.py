d9a71d73803a604121981c90395c26e1b3cecf03
x-channel <cairecr@hotmail.com>
2019-11-24 16:15:22 -0300

    Add files via upload

---
+++ pares/server/config.py
@@ -0,0 +1,14 @@
+# arquivo de comfiguracao do servidor
+
+import socket
+#import save
+
+
+host_name = socket.gethostname()
+
+my_host = socket.gethostbyname(host_name)
+my_port = 8001
+
+key1 = 3
+key2 = 5
+public = 15
---
+++ pares/server/main.py
@@ -0,0 +1,125 @@
+import socket
+import threading
+from config import * #abre as configuracoes
+import pandas as pd
+import time
+
+ENCODING = 'utf-8'
+
+
+def openUsers():
+    col = pd.read_csv('colaborador.csv', header = 0)
+    col.set_index('usuario', inplace = True)
+    emp = pd.read_csv('empregador.csv', header = 0)
+    emp.set_index('projeto', inplace = True)
+    con = pd.read_csv('controle.csv', header = 0)
+    return col, emp, con
+
+def saveUsers():
+    pass
+
+
+class Receiver(threading.Thread):
+
+    def __init__(self, my_host, my_port):
+        threading.Thread.__init__(self, name="messenger_receiver")
+        self.host = my_host
+        self.port = my_port
+
+    def listen(self):
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((self.host, self.port))
+
+        while True:
+            data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+            print ("received message:", data)
+
+    def run(self):
+        self.listen()
+
+
+class Sender(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self, name="messenger_sender")
+        self.host = "0.0.0.0"
+        self.port = "6666"
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+        self.message = ""
+
+    def run(self):
+        while True:
+            message = bytes(self.message, 'utf-8')
+            if self.message != "":
+                try:
+                    print(self.message)
+                    print(message)
+                    self.sock.sendto(message, (self.host, self.port))
+                    self.message = ""
+                except ValueError:
+                    print("erro")
+                    #print(ValueError)
+                    self.message = ""
+
+class PeerServer(Receiver):
+    def __init__(self,my_host,my_port,sender):
+        threading.Thread.__init__(self, name="Server")
+        self.host = my_host
+        self.port = my_port
+        self.sender = sender
+
+    def listen(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((self.host, self.port))
+
+        while True:
+            data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+            datas = data.decode()
+            print ("received message:", addr, datas)
+            
+
+            # O cliente inicia a comunicacao
+            if datas[:5] == 'Hello':
+                self.sender.host = addr[0]
+                self.sender.port = int(datas.split()[-1])
+                print(self.sender.port)
+                #time.sleep(2)
+                self.sender.message = "%i"%public
+
+            # O Server manda a chave publica se as credenciais forem validas
+            elif datas[:6] == 'ACESS:':
+                pass
+
+            # O Empregador pede novas maquinas
+            elif datas[:4] == 'BEG!':
+                pass
+
+            # O Server pede a confirmacao dos clientes para manterem logados
+            elif datas == 'AMIGO EU ESTOU AQUI!':
+                pass
+
+def main(my_host,my_port):
+    print("@:\t\t", my_host)
+    print("port:\t\t", my_port)
+    sender = Sender()
+    sender.start()
+    own = PeerServer(my_host,my_port,sender)
+    own.start()
+
+def makehost(my_host, my_port):
+    receiver = Receiver(my_host, my_port)
+    oh = receiver.start()
+    return oh
+
+th = None
+
+if __name__ == '__main__':
+    print("Host:\t\t", host_name)
+    main(my_host, my_port)
+    #ohost = makehost(my_host, my_port)
+    col,emp,con = openUsers()
+    co = pd.DataFrame(columns = ["usuario","ip","portas","disponibilidade","projetos","RAM","CPU"])#colaboradores online
+    em = pd.DataFrame(columns = ["projeto","ip","porta","orcamento","contrato","colaboradores"])#projetos online
+    print()
+    #th = vizinhanca()
