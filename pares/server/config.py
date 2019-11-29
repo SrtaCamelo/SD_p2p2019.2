5cfab3efc04ec73020c0d07ceb9c5302954e4e2e
Unknown <pedrojudo74@hotmail.com>
2019-11-29 16:17:22 -0300

    Organizando Saidas 2

--- pares/server/config.py
+++ pares/server/config.py
@@ -7,7 +7,7 @@ import socket
 host_name = socket.gethostname()
 
 my_host = socket.gethostbyname(host_name)
-my_host = "10.248.224.98" #IP DO PC DE PEDRO
+my_host = "10.246.227.101" #IP DO PC DE PEDRO
 my_port = 8001
 
 key1 = 3
